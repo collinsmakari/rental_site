788eea5f918484bd83dd804d7b83882105fa34f5
Collins Makari <collinsmakari6@gmail.com>
2026-08-18 20:31:10 +0300

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Layout from "./components/layout/Layout";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
