f83252a7129403d4081599e1280f9bd3f5c8f5a9
Collins Makari <collinsmakari6@gmail.com>
2026-08-22 20:43:14 +0300

    blog

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
           <Route path="/about" element={<About />} />
 
           <Route path="/blog/understanding-rental-agreements" element={<Blog />} />
+          <Route path="/blog" element={<Blog />} />
 <Route
   path="/properties/:id"
   element={<PropertyDetails />}
